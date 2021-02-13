43129fe998969a1ff2c4e957690a75aeee7136dc
Jigar Patel <jigar.moyo@gmail.com>
2021-02-13 22:45:13 +0000

    adding lambda function code for alert feature

---
+++ back-end/food-inspection-lambda/function/index.js
@@ -0,0 +1,27 @@
+const AWSXRay = require('aws-xray-sdk-core')
+const AWS = AWSXRay.captureAWS(require('aws-sdk'))
+const alert = require('./business-logic/check-inspection-data')
+
+// Create client outside of handler to reuse
+const lambda = new AWS.Lambda()
+
+// Handler
+exports.handler = async function(event, context) {
+  console.log('***hello world***')
+  console.log('## ENVIRONMENT VARIABLES: ' + serialize(process.env))
+  console.log('## CONTEXT: ' + serialize(context))
+  console.log('## EVENT: ' + serialize(event))
+
+  alert.checkDataAndSendAlerts()
+  
+  return getAccountSettings()
+}
+
+// Use SDK client
+var getAccountSettings = function(){
+  return lambda.getAccountSettings().promise()
+}
+
+var serialize = function(object) {
+  return JSON.stringify(object, null, 2)
+}
